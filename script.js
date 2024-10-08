7b3fefed440e820f5b0b5732f92e5bb859f4662d
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-10-08 10:23:46 +0000

    g

--- script.js
+++ script.js
@@ -14,15 +14,6 @@ druhy.addEventListener('click', function(){
 })
 
 
-<script>
-        document.getElementById("timeButton").addEventListener("click", function() {
-            const currentTime = new Date().toLocaleTimeString();
-            document.getElementById("output").innerText = "Aktuální čas: " + currentTime;
-        });
 
-        document.getElementById("dateButton").addEventListener("click", function() {
-            const currentDate = new Date().toLocaleDateString();
-            document.getElementById("output").innerText = "Aktuální datum: " + currentDate;
-        });
     
         
