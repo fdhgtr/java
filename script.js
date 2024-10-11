1945f93ec320d31b9ccfb5a28acd6285a2f027df
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-10-11 08:25:25 +0000

    l

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let prvni = document.getElementById('prvni')
 
 prvni.addEventListener('click', function(){
@@ -23,4 +24,21 @@ function zapisText() {
     document.getElementById("novyText").innerText = "Toto je nový text!";
 }
     
-        
+function hellouser() {
+    const userName = document.getElementById('user').value;
+    if (userName) {
+        alert('Ahoj, ' + userName + '!');
+    } else {
+        alert('Prosím, zadejte své jméno.');
+    }
+}
+
+function changeHeadingText() {
+    document.getElementById('heading').textContent = 'Text úkolu se změnil!';
+}
+
+function resetHeadingText() {
+    document.getElementById('heading').textContent = 'Úkol 7';
+}
+
+      
